6725b7db51ce75c8b83c4a898bbb06c426990072
guiraikou <guicarvalho@hotmail.com.br>
2024-09-09 18:10:34 -0300

    Feature: salvando cookies e criando middleware

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import logo from '/public/logo.svg';
 import Link from "next/link";
 import { api } from "@/services/api";
 import { redirect } from "next/navigation";
+import { cookies } from "next/headers"; 
 
 export default function Home() {
 
@@ -24,6 +25,15 @@ export default function Home() {
       })
 
       console.log("Data:", response.data);
+
+      const expressTime = 60 * 60 * 24 * 30 * 1000;
+
+      cookies().set("login", response.data.token, {
+        maxAge: expressTime,
+        path: "/",
+        httpOnly: false,
+        secure: process.env.NODE_ENV === "production"
+      })
 
       if (!response.data.token) {
         return;
---
+++ src/lib/cookieServer.ts
@@ -0,0 +1,7 @@
+import { cookies } from "next/headers";
+
+export function getCookieServer(){
+    const token = cookies().get("login")?.value;
+
+    return token || null;
+}
